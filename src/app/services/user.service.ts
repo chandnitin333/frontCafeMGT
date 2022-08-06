85685f89fa7c048271fe37a7afbca7e0678594ef
Nitin Chandekar <chand.nitin333@gmail.com>
2022-08-07 00:38:01 +0530

    front End

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,43 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {environment} from '../../environments/environment'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  url = environment.apiUrl;
+
+  constructor(private httpClient : HttpClient) { }
+
+  signup(data:any){
+      return this.httpClient.post(this.url+'user/signup',data,
+      {
+        headers:new HttpHeaders().set('Content-Type',"application/json")
+
+      }
+      )
+
+  }
+
+  forgotPassword(data:any){
+    return this.httpClient.post(this.url+'user/forgetPassword',data,{headers:new HttpHeaders().set('Content-Type',"application/json")})
+  }
+
+
+
+  login(data:any){
+    return this.httpClient.post(this.url+'user/login',
+    data,
+    {headers:new HttpHeaders().set('Content-Type',"application/json")}
+    );
+  }
+
+
+  checkToken(){
+    return this.httpClient.post(this.url+'user/checkToken',{headers:new HttpHeaders().set('Content-Type',"application/json")})
+
+  }
+
+
+}
